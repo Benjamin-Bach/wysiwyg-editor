6fa676032ea9f95f481478f6d04a29a853bc87f1
Benjamin-Bach <mail@benjaminbach.fr>
2018-03-25 00:36:30 +0100

    better style, new work around class editorButton

--- wysiwg.js
+++ wysiwg.js
@@ -9,8 +9,11 @@ document.write('<textarea class=" '  + pjn + ' "><h1 class="title">text html</h1
 
 document.write('<style>');
 document.write('.' + pjn + '{background:#ddd;display:block;width:100%;height:20vh;}');
-document.write('.' + pjn + '-body{background:#ddf;min-height:10rem;}');
+document.write('.' + pjn + '-body{box-shadow:inset 0 0 10px rgba(0,0,0,0.3); min-height:10rem;margin:0px; padding:0.3rem;}');
+document.write('.' + pjn + '-editor > ul {margin:0px;}');
+document.write('.' + pjn + '-editor > ul li{display:inline-block; padding:0.5rem 1rem; }');
 document.write('</style>');
+
 
 // init editor
 class editorInit {
@@ -23,7 +26,7 @@ class editorInit {
   }
 
   get buildEditor() {
-    this.buildToolsBar();
+    this.buildEditorMarkup();
     this.editorBody = this.textarea.previousElementSibling.querySelector('.' + pjn + '-body');
     this.buildSwitchLink();
     this.fromTextAreaToEditor();
@@ -35,7 +38,7 @@ class editorInit {
     });
   }
 
-  buildToolsBar(){
+  buildEditorMarkup(){
     this.textarea.insertAdjacentHTML('beforebegin', this.editorTemplate);
     this.editorBody = this.textarea.previousElementSibling.querySelector('.' + pjn + '-body');
     this.editorBody.contentEditable = true;
@@ -70,11 +73,25 @@ class editorButton {
 
   get buildButton() {
     for (let toolsBar of this.toolsBars) {
-      toolsBar.innerHTML = toolsBar.innerHTML + '<li>' + this.name + '</li>';
+      let btn = document.createElement('LI');
+      let text = document.createTextNode(this.name);
+      let getSelected = this.getSelected;
+      btn.appendChild(text);
+      let $this = this;
+      btn.addEventListener('click', function(){
+        getSelected($this);
+      });
+      toolsBar.appendChild(btn);
     }
+  }
+
+  getSelected($this){
+      let selected = document.getSelection();
+      let range = selected.getRangeAt(0);
+      console.log(range);
   }
 
 }
 
-new editorButton('test').buildButton;
-new editorButton('test 2').buildButton;
+new editorButton('italic').buildButton;
+new editorButton('bold').buildButton;
